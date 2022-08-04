5a51c82da1fc5f0740c13ce61fe5ebd46e4a3faa
Student <bmadlezi@gmail.com>
2022-08-05 00:28:42 +0200

    added person class

--- home_page.dart
+++ home_page.dart
@@ -1,4 +1,13 @@
 void main() {
   String name = 'buhlebezwe';
   print(name);
+  Person buhle = Person(name: 'buhlebezwe', age: 21);
+  print('name: ${buhle.name}, age : ${buhle.age}');
+}
+
+class Person {
+  late String name;
+  late int age;
+
+  Person({required this.name, required this.age});
 }
