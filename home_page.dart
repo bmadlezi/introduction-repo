21ea186045bb58b9ad6dcaa9a01cfaeae0655732
Student <bmadlezi@gmail.com>
2022-08-05 00:45:26 +0200

    print

--- home_page.dart
+++ home_page.dart
@@ -3,6 +3,7 @@ void main() {
   print(name);
   Person buhle = Person(name: 'buhlebezwe', age: 21);
   print('name: ${buhle.name}, age : ${buhle.age}');
+  print('hello this is my second section');
 }
 
 class Person {
